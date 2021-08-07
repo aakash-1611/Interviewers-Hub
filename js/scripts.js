649bd8c5645f85636cafb046827298b63f9d5fd7
Aakash bhatnagar <aakash@Aakashs-MacBook-Air.local>
2021-08-08 01:14:40 +0530

    unwanted files

--- js/scripts.js
+++ js/scripts.js
@@ -132,20 +132,20 @@
 
     /* Back To Top Button */
     // create the back to top button
-    $('body').prepend('<a href="body" class="back-to-top page-scroll">Back to Top</a>');
-    var amountScrolled = 700;
-    $(window).scroll(function() {
-        if ($(window).scrollTop() > amountScrolled) {
-            $('a.back-to-top').fadeIn('500');
-        } else {
-            $('a.back-to-top').fadeOut('500');
-        }
-    });
+ //    $('body').prepend('<a href="body" class="back-to-top page-scroll">Back to Top</a>');
+ //    var amountScrolled = 700;
+ //    $(window).scroll(function() {
+ //        if ($(window).scrollTop() > amountScrolled) {
+ //            $('a.back-to-top').fadeIn('500');
+ //        } else {
+ //            $('a.back-to-top').fadeOut('500');
+ //        }
+ //    });
 
 
-	/* Removes Long Focus On Buttons */
-	$(".button, a, button").mouseup(function() {
-		$(this).blur();
-	});
+	// /* Removes Long Focus On Buttons */
+	// $(".button, a, button").mouseup(function() {
+	// 	$(this).blur();
+	// });
 
 })(jQuery);
